0473785806a952f29b45910dcc0939a039cb43c2
Matt Myers <mattclone@gmail.com>
2018-11-13 15:06:52 -0800

    client and server properly connected

--- src/org/titlepending/server/Server.java
+++ src/org/titlepending/server/Server.java
@@ -1,21 +1,24 @@
 package org.titlepending.server;
 
-import org.titlepending.client.Client;
+import org.lwjgl.Sys;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Server {
 
     private static final int PORT = 8000;
     private static final int PLIMIT = 8;
-    private static ConcurrentLinkedQueue<Player> players= new ConcurrentLinkedQueue<>();
+
+    //TODO change to concurrent
+    private static final List<Socket> players = Collections.synchronizedList(new ArrayList<>());
 
     public static void main(String[] args) throws IOException{
-
 
         System.out.println("Game server started");
 
@@ -23,6 +26,12 @@ public class Server {
 
         while(true){
             // Game logic goes here
+
+            if(players.size()!=8){
+
+            }else{
+
+            }
         }
 
     }
@@ -31,60 +40,48 @@ public class Server {
         ServerSocket listener;
         public Handler() throws IOException{
             listener = new ServerSocket(PORT);
+            boolean done = false;
             System.out.println("Listening for new clients.");
-            try{
-                while(true){
-                    new Clients(listener.accept()).start();
-                }
-            }catch (IOException e){
-                e.printStackTrace();
-            }finally {
-                listener.close();
+
+            while(!done){
+                new Players(listener.accept()).start();
+                if(players.size()==8)
+                    done=true;
             }
+            System.out.println("Maximum players reached");
+            listener.close();
+
         }
     }
 
-    private static class Clients extends Thread{
+    private static class Players extends Thread{
         private Socket socket;
         private ObjectInputStream in;
         private ObjectOutputStream out;
         private Player player;
-        public Clients(Socket socket) throws IOException{
+        public Players(Socket socket) throws IOException{
             this.socket=socket;
-            System.out.println("creating output stream.");
-            out = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("creating input stream.");
-            in = new ObjectInputStream(socket.getInputStream());
-
-
+            players.add(socket);
         }
 
         public void run(){
             System.out.println("New player connected.");
             try{
-                System.out.println("Attempting to create player.");
-                player = new Player(ThreadLocalRandom.current().nextInt(),out,in);
-                players.add(player);
-                System.out.println("Player created attempting to send response to client.");
+                out = new ObjectOutputStream(socket.getOutputStream());
                 out.write(2);
                 out.flush();
-                System.out.println("Connected sending signal to move to playing state.");
-
-
-            } catch (IOException e){
+            }catch (IOException e){
                 e.printStackTrace();
             }finally {
-                   players.remove(player);
-                try{
-                    in.close();
-                    out.close();
-                    socket.close();
+                System.out.println("Disconnecting Players");
+                try {
+                     out.close();
+                     socket.close();
+                     players.remove(socket);
                 }catch (IOException e){
-                    e.printStackTrace();
+                     e.printStackTrace();
                 }
             }
-
-
 
         }
     }
