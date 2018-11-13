6938f5de5ec3792b8b31f8bec8c889b23c02e3bb
Matt Myers <mattclone@gmail.com>
2018-11-13 15:55:14 -0800

    removed unused classes, moved client thread to shared folder

---
+++ src/org/titlepending/shared/ClientThread.java
@@ -0,0 +1,40 @@
+package org.titlepending;
+
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class ClientThread extends Thread{
+    private Socket socket;
+    private ObjectInputStream in;
+    public ClientThread (Socket socket) throws IOException {
+        this.socket=socket;
+    }
+
+    public void run(){
+        System.out.println("New player connected.");
+        boolean done = false;
+        try{
+            in = new ObjectInputStream(socket.getInputStream());
+            while (!done){
+                try{
+                    Object input = in.readObject();
+                }catch (ClassNotFoundException e){
+                    e.printStackTrace();
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("Disconnecting Players");
+            try {
+                in.close();
+                socket.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
