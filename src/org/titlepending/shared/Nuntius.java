e751c24c4e1aecce9d02933bd9e757baf88c5e6e
Matt Myers <mattclone@gmail.com>
2018-11-19 15:21:22 -0800

    added object for client server messaging

---
+++ src/org/titlepending/shared/Nuntius.java
@@ -0,0 +1,19 @@
+package org.titlepending.shared;
+
+/**
+ * object that is sent across TCP/IP socket for communication in client server pair.
+ */
+public class Nuntius {
+    private boolean forServer;
+    private int stateTransition;
+
+    public Nuntius(){}
+    
+    public  void setForServer(boolean forServer){this.forServer = forServer;}
+
+    public void setStateTransition(int stateTransition){this.stateTransition = stateTransition;}
+
+    public int getStateTransition(){return stateTransition;}
+
+    public boolean getForServer(){return forServer;}
+}
