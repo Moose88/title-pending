d298891463a4ebce51037665bbbc937d4e52c913
Matthew Bourgoine <mbourgoine21@gmail.com>
2018-11-21 14:06:11 -0800

    Added stats to the ships and each ship part array

--- src/org/titlepending/client/states/LobbyState.java
+++ src/org/titlepending/client/states/LobbyState.java
@@ -9,8 +9,6 @@ import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 import org.titlepending.client.Client;
 
-import java.util.prefs.BackingStoreException;
-
 public class LobbyState extends BasicGameState {
     private Client client;
     private int selection;
@@ -30,6 +28,42 @@ public class LobbyState extends BasicGameState {
     private int setCannons;
     private int setCrew;
 
+    private int[] haulMod = new int[3];
+    private int[] sailMod = new int[3];
+    private int[] cannonMod = new int[3];
+    private int[] crewMod = new int[3];
+
+    private int totalMod;
+
+    private Image[] haul = new Image[3];
+    private Image[] sails = new Image[3];
+    private Image[] cannons = new Image[3];
+    private Image[] crew = new Image[3];
+
+    private String[] haulString = new String[3];
+    private String[] sailString = new String[3];
+    private String[] cannonString = new String[3];
+    private String[] crewString = new String[3];
+
+    private static SpriteSheet ship_RSC_96 = new SpriteSheet(ResourceManager.getImage(Client.SHIP_RSC), 96, 96);
+    private static SpriteSheet ship_RSC_32 = new SpriteSheet(ResourceManager.getImage(Client.SHIP_RSC), 64, 16);
+
+    private static Image smallHaul;
+    private static Image medHaul;
+    private static Image lgHaul;
+
+    private static Image oneSail;
+    private static Image twoSails;
+    private static Image threeSails;
+
+    private static Image oneCannon;
+    private static Image twoCannons;
+    private static Image threeCannons;
+
+    private static Image crewOne;
+    private static Image crewTwo;
+    private static Image crewThree;
+
     public void init(GameContainer container, StateBasedGame game)
             throws SlickException{
 
@@ -46,7 +80,68 @@ public class LobbyState extends BasicGameState {
         setHaul = (int) savedShip.getNumber("haul",0);
         setSails = (int) savedShip.getNumber("sails",0);
         setCannons = (int) savedShip.getNumber("cannons",0);
-        setCannons = (int) savedShip.getNumber("crew",0);
+        setCrew = (int) savedShip.getNumber("crew",0);
+
+        if(Client.DEBUG)
+            System.out.println("Haul: " + setHaul + " Sails: " + setSails + " Cannons: " + setCannons + " Crew: " + setCrew);
+
+        // Haul Images
+
+        medHaul = ship_RSC_96.getSubImage(0, 0).getScaledCopy(3f);
+        medHaul.rotate(90f);
+
+        // Sail Images
+
+        oneSail = ship_RSC_32.getSubImage(3, 0).getScaledCopy(3f);
+        oneSail.rotate(90f);
+
+        // Cannon Images
+
+        // Crew Images
+
+        haul[2] = smallHaul;
+        haul[1] = medHaul;
+        haul[0] = lgHaul;
+
+        haulString[2] = "Small Haul";
+        haulString[1] = "Medium Haul";
+        haulString[0] = "Large Haul";
+
+        haulMod[2] = 3;
+        haulMod[1] = 5;
+        haulMod[0] = 7;
+
+        sails[2] = oneSail;
+        sails[1] = twoSails;
+        sails[0] = threeSails;
+
+        sailString[2] = "One Sail";
+        sailString[1] = "Two Sails";
+        sailString[0] = "Three Sails";
+
+        sailMod[2] = 3;
+        sailMod[1] = 5;
+        sailMod[0] = 7;
+
+        cannons[2] = oneCannon;
+        cannons[1] = twoCannons;
+        cannons[0] = threeCannons;
+
+        cannonString[2] = "One Cannon";
+        cannonString[1] = "Two Cannons";
+        cannonString[0] = "Three Cannons";
+
+        cannonMod[2] = 3;
+        cannonMod[1] = 5;
+        cannonMod[0] = 7;
+
+        crew[2] = crewOne;
+        crew[1] = crewTwo;
+        crew[0] = crewThree;
+
+        crewString[2] = "Zog!";
+        crewString[1] = "Other guy!";
+        crewString[0] = "Non Binary Gendered Creature thing!";
 
     }
 
@@ -98,24 +193,64 @@ public class LobbyState extends BasicGameState {
          *  sails, cannons, etc
          */
 
+
+
         //Draw the crew on the left
         /**
          *  TODO: Render a splash of the crew image
          */
+
+
 
         //Display the stats on the right
         /**
          *  TODO: Display the stats on the right side
          */
 
+        Color someBlue = new Color(66, 134, 244);
+        Color someOther = new Color(66, 0, 250);
+        Color someOther2 = new Color(66, 134, 0);
 
+        g.pushTransform();
+        g.scale(2f,2f);
+
+        String lobbyStats = "Modifiers: " + "\n" +
+                "\n" +
+                "Haul: " + haulMod[setHaul] + "\n" +
+                "\n" +
+                "Sails: " + sailMod[setSails] + "\n" +
+                "\n" +
+                "Cannons: " + cannonMod[setCannons] + "\n" +
+                "\n" +
+                "Crew: " + crewString[setCrew] + "\n" +
+                "\n" +
+                "Total Modifier: " + totalMod;
+
+        if(setCrew == 0)
+            g.setColor(someBlue);
+        else if(setCrew == 1)
+            g.setColor(someOther);
+        else if(setCrew == 2)
+            g.setColor(someOther2);
+
+        float x1 = ((client.ScreenWidth*.405f)-(g.getFont().getWidth(crewString[1])/3f));
+        float y1 = ((client.ScreenHeight*.085f)-(g.getFont().getHeight(lobbyStats)/3f));
+
+        g.setFont(Client.fontStandard);
+        g.fillRect(x1, y1, g.getFont().getWidth(lobbyStats)+50, g.getFont().getHeight(lobbyStats)+10);
+        g.setColor(Color.white);
+        g.drawString(lobbyStats, x1+10, y1+10);
+        g.popTransform();
+
+
+        //Display the Menu strings/items
         int yTop = (int) (client.ScreenHeight * 0.6); // two thirds down the string
         int itemSpace = 95;
 
-        drawMenuItem("< " + setHaul + " >", yTop, isSelected(HAUL));
-        drawMenuItem("< " + setSails + " >", yTop + SAILS * itemSpace, isSelected(SAILS));
-        drawMenuItem("< " + setCannons + " >", yTop + CANNONS * itemSpace, isSelected(CANNONS));
-        drawMenuItem("< " + setCrew + " >", yTop + CREW * itemSpace, isSelected(CREW));
+        drawMenuItem("< " + haulString[setHaul] + " >", yTop, isSelected(HAUL));
+        drawMenuItem("< " + sailString[setSails] + " >", yTop + SAILS * itemSpace, isSelected(SAILS));
+        drawMenuItem("< " + cannonString[setCannons] + " >", yTop + CANNONS * itemSpace, isSelected(CANNONS));
+        drawMenuItem("< " + crewString[setCrew] + " >", yTop + CREW * itemSpace, isSelected(CREW));
         drawMenuItem("Ready", yTop + READY * itemSpace, isSelected(READY));
         drawMenuItem("Exit", yTop + BACK * itemSpace, isSelected(BACK));
 
@@ -124,12 +259,19 @@ public class LobbyState extends BasicGameState {
 
     public void update(GameContainer container, StateBasedGame game,
                        int delta) throws SlickException{
-
+        totalMod = haulMod[setHaul] + sailMod[setSails] + cannonMod[setCannons];
     }
 
     @Override
     public void keyPressed(int key, char c){
-        System.out.println(selection);
+
+        /**
+         *  TODO: If total modifier goes over 15, you cannot go to the bigger option, only the lower option.
+         */
+        
+        if(Client.DEBUG)
+            System.out.println("Haul: " + setHaul + " Sails: " + setSails + " Cannons: " + setCannons + " Crew: " + setCrew);
+
         if(key == Input.KEY_ESCAPE){
             backPressed();
         }
@@ -158,7 +300,7 @@ public class LobbyState extends BasicGameState {
 
         // Left and right selection for Haul
         if(key == Input.KEY_LEFT && selection == HAUL){
-            if(setHaul >= 4)
+            if(setHaul >= 2)
                 setHaul = 0;
             else
                 setHaul++;
@@ -168,7 +310,7 @@ public class LobbyState extends BasicGameState {
 
         if(key == Input.KEY_RIGHT && selection == HAUL){
             if(setHaul <= 0)
-                setHaul = 4;
+                setHaul = 2;
             else
                 setHaul--;
             savedShip.setNumber("haul", setHaul);
@@ -177,7 +319,7 @@ public class LobbyState extends BasicGameState {
 
         // Left and right selection for Sails
         if(key == Input.KEY_LEFT && selection == SAILS){
-            if(setSails >= 4)
+            if(setSails >= 2)
                 setSails = 0;
             else
                 setSails++;
@@ -187,7 +329,7 @@ public class LobbyState extends BasicGameState {
 
         if(key == Input.KEY_RIGHT && selection == SAILS){
             if(setSails <= 0)
-                setSails = 4;
+                setSails = 2;
             else
                 setSails--;
             savedShip.setNumber("sails", setSails);
@@ -196,7 +338,7 @@ public class LobbyState extends BasicGameState {
 
         // Left and right selection for Cannons
         if(key == Input.KEY_LEFT && selection == CANNONS){
-            if(setCannons >= 4)
+            if(setCannons >= 2)
                 setCannons = 0;
             else
                 setCannons++;
@@ -206,7 +348,7 @@ public class LobbyState extends BasicGameState {
 
         if(key == Input.KEY_RIGHT && selection == CANNONS){
             if(setCannons <= 0)
-                setCannons = 4;
+                setCannons = 2;
             else
                 setCannons--;
             savedShip.setNumber("cannons", setCannons);
@@ -215,7 +357,7 @@ public class LobbyState extends BasicGameState {
 
         // Left and right selection for Crew
         if(key == Input.KEY_LEFT && selection == CREW){
-            if(setCrew >= 4)
+            if(setCrew >= 2)
                 setCrew = 0;
             else
                 setCrew++;
@@ -225,7 +367,7 @@ public class LobbyState extends BasicGameState {
 
         if(key == Input.KEY_RIGHT && selection == CREW){
             if(setCrew <= 0)
-                setCrew = 4;
+                setCrew = 2;
             else
                 setCrew--;
             savedShip.setNumber("crew", setCrew);
