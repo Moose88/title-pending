c18e4976c70df966b56b2c7ac345863c885ae697
Matthew Bourgoine <mbourgoine21@gmail.com>
2018-11-28 12:41:43 -0800

    Modified and completed the rejecting state

--- src/org/titlepending/client/states/RejectedState.java
+++ src/org/titlepending/client/states/RejectedState.java
@@ -1,16 +1,22 @@
 package org.titlepending.client.states;
 
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import jig.ResourceManager;
+import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 import org.titlepending.client.Client;
+import org.titlepending.client.Updates;
 
 public class RejectedState extends BasicGameState{
-    int timer;
+
+    private Client client;
+    private int timer;
+
     public void init(GameContainer container, StateBasedGame game)
             throws SlickException{
+        this.client = (Client)game;
 
     }
 
@@ -22,6 +28,16 @@ public class RejectedState extends BasicGameState{
 
     public void render(GameContainer container, StateBasedGame game,
                        Graphics g) throws SlickException{
+        Client client = (Client) game;
+
+        g.drawImage(ResourceManager.getImage(Client.FRONT_MENU_RSC).getScaledCopy(3f), 0, 0);
+        String string1 = "Server is full, or does not exist.";
+        String string2 = "Please press any button to return to the front Menu.";
+
+        g.setFont(client.fontMenu);
+        g.setColor(Color.black);
+        g.drawString(string1, client.ScreenWidth/2 - g.getFont().getWidth(string1)/2, client.ScreenHeight/2 - g.getFont().getHeight(string1)/2);
+        g.drawString(string2, client.ScreenWidth/2 - g.getFont().getWidth(string2)/2, client.ScreenHeight/2 + g.getFont().getHeight(string2)/2);
 
     }
 
@@ -30,11 +46,20 @@ public class RejectedState extends BasicGameState{
         timer -= delta;
         if(timer<=0){
             Client client = (Client)game;
+            ResourceManager.getMusic(Client.TITLE_MUSIC).loop();
             client.enterState(Client.MAINMENUSTATE);
         }
     }
 
     public int getID(){return Client.REJECTSTATE; }
 
+    @Override
+    public void keyPressed(int key, char c){
+        if(true){
+            System.out.println(key);
+            ResourceManager.getMusic(Client.TITLE_MUSIC).loop();
+            client.enterState(Client.MAINMENUSTATE);
+        }
+    }
 
 }
